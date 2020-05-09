7e25a182b8f531b037f6cce1f854290b469931f8
Mohit Chandana <chandanamohit@gmail.com>
2020-05-10 03:43:39 +0530

    Delete HelloWorld component and create AppHeader component and add Bootstrap Nav component

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,20 @@
 <template>
   <div id="app">
-    <div>
-      <b-button>Button</b-button>
-      <b-button variant="danger">Button</b-button>
-      <b-button variant="success">Button</b-button>
-      <b-button variant="outline-primary">Button</b-button>
-    </div>
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <AppHeader />
+    <h1>Welcome to Vue JS</h1>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import AppHeader from '@/components/AppHeader'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    AppHeader
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <b-navbar toggleable="lg" type="dark" variant="success">
+      <b-navbar-brand href="#">Product Manager</b-navbar-brand>
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AppHeader',
+  data(){
+    return {
+      
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
