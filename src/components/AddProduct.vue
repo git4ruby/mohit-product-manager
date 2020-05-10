d883537b57967d4ae80b1b0f8b16e526b02fb334
Mohit Chandana <chandanamohit@gmail.com>
2020-05-10 18:04:33 +0530

    Create input form

--- src/components/AddProduct.vue
+++ src/components/AddProduct.vue
@@ -1,15 +1,53 @@
 <template>
   <b-col md="4" class="m-3">
     <b-card header="AddProduct">
-      <b-card-text>
-      </b-card-text>
-      <b-button href="#" variant="primary">Add Product</b-button>
+      <b-card-body>
+        <b-form autocomplete="off">
+          <b-form-group
+            label="Name"
+          >
+            <b-form-input v-model= "form.name" id="name" name= 'name' trim>
+            </b-form-input>
+          </b-form-group>
+          <b-form-group
+            label="Price ($)"
+          >
+            <b-form-input v-model= "form.price" id="price" name= 'price' trim>
+            </b-form-input>
+          </b-form-group>
+          <b-form-group
+            label="Brand"
+          >
+            <b-form-input v-model= "form.brand" id="brand" name= 'brand' trim>
+            </b-form-input>
+          </b-form-group>
+          <b-form-group label="Inventory">
+            <b-form-radio v-model= "form.inventoryStatus" name="inventoryStatus" value="true">In Stock</b-form-radio>
+            <b-form-radio v-model= "form.inventoryStatus" name="inventoryStatus" value="false">Out of Stock</b-form-radio>
+          </b-form-group>
+        </b-form>
+      </b-card-body>
+      <b-button block variant="primary" @click="addProduct">Add Product</b-button>
     </b-card>
   </b-col>
 </template>
 
 <script>
 export default {
+  data(){
+    return {
+      form: {
+        name: '',
+        price: '',
+        inventoryStatus: ''
+      }
+    }
+  },
+  methods: {
+    addProduct(){
+      console.log(this.form)
+    }
+  }
 
 }
 </script>
