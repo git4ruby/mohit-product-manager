c8bb7d19d874624cc3d5124c1ab5c6a586b7784d
Mohit Chandana <chandanamohit@gmail.com>
2020-05-10 19:19:46 +0530

    Fetch data from json API

--- src/App.vue
+++ src/App.vue
@@ -14,12 +14,33 @@
 import AppHeader from '@/components/AppHeader'
 import ListProduct from '@/components/ListProduct'
 import AddProduct from '@/components/AddProduct'
+import axios from 'axios'
+
 export default {
   name: 'App',
   components: {
     AppHeader,
     AddProduct,
     ListProduct
+  },
+  data(){
+    return{
+      productList: []
+    }
+  },
+  methods: {
+    async getProductList(){
+      try {
+        let result = await axios.get('http://localhost:3000/products')
+        console.log(result.data)
+        this.productList = result.data
+      } catch (error) {
+        console.log(error)
+      }
+    }
+  },
+  mounted(){
+    this.getProductList();
   }
 }
 </script>
